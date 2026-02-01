9f1f42543deeee3a9c962107e7226aab01fcad22
josedvega <davarela231@gmail.com>
2026-02-01 00:20:25 -0600

    Team activity

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,7 @@
 const { text } = require("express");
 const utilities = require("../utilities/");
-const accountModel = require("../models/account-model")
+const accountModel = require("../models/account-model");
+const bcrypt = require("bcryptjs");
 // const pswdBtn = document.querySelector("#pswdBtn"); Unable to retrieve the pswdBtn element
 
 async function buildLogin(req, res) {
@@ -18,38 +19,60 @@ async function buildRegister(req, res, next) {
 }
 
 /* ****************************************
-*  Process Registration
-* *************************************** */
+ *  Process Registration
+ * *************************************** */
 async function registerAccount(req, res) {
-  let nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+  let nav = await utilities.getNav();
+  const {
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password,
+  } = req.body;
+
+  // Hash the password before storing
+  let hashedPassword;
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10);
+  } catch (error) {
+    req.flash(
+      "notice",
+      "Sorry, there was an error processing the registration.",
+    );
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    });
+  }
 
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
-  )
+    hashedPassword
+  );
 
   if (regResult) {
     req.flash(
       "notice",
-      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-    )
+      `Congratulations, you\'re registered ${account_firstname}. Please log in.`,
+    );
     res.status(201).render("account/login", {
       title: "Login",
       nav,
-    })
+    });
   } else {
-    req.flash("notice", "Sorry, the registration failed.")
+    req.flash("notice", "Sorry, the registration failed.");
     res.status(501).render("account/register", {
       title: "Registration",
       nav,
-    })
+    });
   }
 }
 
-// togglePassword function not working 
+// togglePassword function not working
 
 /* function togglePassword(button) {
   const pswdInput = document.getElementById("account_password");
